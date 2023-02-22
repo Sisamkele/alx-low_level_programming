05fa201451af1e8b18f8980585fb80f126cd4083
Sisamkele <sisamkelemb9@gmail.com>
2023-02-22 02:25:45 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,28 @@
 #include "main.m"
 
 /**
- * print_sign - print the sign of a num
- * @n: the num to be checked for
+ * print_sign - print the sign of a mnum
+ * @n: the number of the signs to be printed
  * Returns 1 and prints 0 if n is zero
  * -1 and prints -iv n if n is less than zero
  */
 
 int print_sign(int n)
 {
-	if ( n > 0)
+	if (n > 0)
 	{
-		_putchar('+');
-		return(1);
+		_putchar ('+');
+		return (1);
+
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		_putchar ('0');
 		return (0);
 	}
 	else
 	{
-		_putchar('_')
-		return(-1);
+		_putchar ('_')
+		return (-1);
 	}
 }
