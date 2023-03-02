4fa4b313bceb6100e7f1964cec3c668f96ade2c5
Sisamkele <sisamkelemb9@gmail.com>
2023-03-02 09:06:01 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,26 +1,50 @@
+#include "main.h"
 #include <stdio.h>
-/*
- * main - the program prints followed by a new line.
- * Return 0 success
- */
-int main(void)
-{
-int n;
-int a[5];
-int *p;
 
-	a[2] = 1024;
-	p = &n;
-/*
- * write your line of code here...
- * Remember:
- * - you are not allowed to use a
- * - you are not allowed to modify p
- * - only one statement
- * - you are not allowed to code anything else than this line of code
+/**
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
+ * Return: void
  */
-	*(p + 5) = 98;
-/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+
+void print_buffer(char *b, int size)
+{
+	int o, j, i;
+
+	o = 0;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (o < size)
+	{
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf("  ");
+			if (i % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
+	}
 }
